2a9d981f2e67b18a14485c57b9cd577c6373592b
Kent Ferolino <kentfero@gmail.com>
2021-04-29 03:48:14 +0800

    Add Layout component. Add styling. Add global _app.js and styles.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,24 +4,24 @@ module.exports = {
         ecmaVersion: 2020,
         sourceType: 'module',
         ecmaFeatures: {
-            jsx: true
-        }
+            jsx: true,
+        },
     },
     settings: {
         react: {
-            version: 'detect'
-        }
+            version: 'detect',
+        },
     },
     env: {
         browser: true,
         amd: true,
-        node: true
+        node: true,
     },
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:jsx-a11y/recommended',
-        'plugin:prettier/recommended'
+        'plugin:prettier/recommended',
     ],
     rules: {
         'prettier/prettier': ['error', {}, { usePrettierrc: true }],
@@ -31,8 +31,8 @@ module.exports = {
             {
                 components: ['Link'],
                 specialLink: ['hrefLeft', 'hrefRight'],
-                aspects: ['invalidHref', 'preferButton']
-            }
-        ]
-    }
+                aspects: ['invalidHref', 'preferButton'],
+            },
+        ],
+    },
 };
